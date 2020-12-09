f8312c5bb010315ecd72b92cd947dbb2b3364da1
Caterina Natali <caterinanatali12@gmail.com>
2020-12-09 12:10:06 +0100

    add loggIn and error fixing

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const admin = require("firebase-admin");
+const firebase = require("firebase");
 const serviceAccount = require("../serviceAccount.json");
 
 const router = express.Router();
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBnd35XIigQN3s6fhh-9WT81g1enQsKVX8",
+    authDomain: "quotes-bck-719d4.firebaseapp.com",
+    projectId: "quotes-bck-719d4",
+    storageBucket: "quotes-bck-719d4.appspot.com",
+    messagingSenderId: "433516584781",
+    appId: "1:433516584781:web:1031800a73ce93057be104"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 quotes = [];
+let loggedIn = false;
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -22,6 +35,23 @@ async function updateQuotes() {
     list.forEach(doc => quotes.push(doc.data()));
 }
 
+router.post("/login", async (req, res) => {
+    try {
+        if (!req.body.email || !req.body.password) {
+            return res.status(400).json({ message: "Bad request, please insert your email or password!" });
+        };
+        const user = await firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password);
+        const token = await user.user.getIdToken();
+        if (token){
+            loggedIn = true;
+        };
+
+        return res.status(201).json({ token });
+    } catch (error) {
+        return res.status(500).send({ error: error.toString() });
+    }
+});
+
 router.get("/quotes", async (req, res) => {
     try {
         await updateQuotes();
@@ -35,7 +65,7 @@ router.get("/quotes/:id", async (req, res) => {
     try {
         const quote = await db.collection('quotes').doc(req.params.id).get();
         if (!quote.data()) {
-            return res.status(404).json({message: "user not found"});
+            return res.status(404).json({ message: "user not found" });
         }
         return res.status(200).json(quote.data());
     } catch (error) {
@@ -44,74 +74,84 @@ router.get("/quotes/:id", async (req, res) => {
 });
 
 router.post("/quotes", async (req, res) => {
-    try {
-        await updateQuotes();
-        if (!req.body.author || !req.body.quote) {
-            return res.status(400).json({message: "Bad request: missing quote or author in the request body!"});
+    if (loggedIn == true) {
+        try {
+            await updateQuotes();
+            if (!req.body.author || !req.body.quote) {
+                return res.status(400).json({ message: "Bad request: missing quote or author in the request body!" });
+            }
+            const newId = genId();
+            let newQuote = {
+                id: newId,
+                quote: req.body.quote,
+                author: req.body.author
+            }
+            db.collection('quotes').doc(newId.toString()).set(newQuote);
+            return res.status(201).json({ message: "Created" });
+        } catch (error) {
+            return res.status(500).send({ error: error.toString() });
         }
-        const newId = genId();
-        let newQuote = {
-            id: newId,
-            quote: req.body.quote,
-            author: req.body.author
-        }
-        db.collection('quotes').doc(newId.toString()).set(newQuote);
-        return res.status(201).json({message: "Created"});
-    } catch (error) {
-        return res.status(500).send({error: error.toString()});
+    }else{
+        return res.status(401).json({message: "Unauthorized.You're not LoggedIn."});
     }
 });
 
 router.patch("/quotes/:id", async (req, res) => {
-    try {
-        quotes.length = 0;
-        const list = await db.collection("quotes").get();
-        list.forEach(doc => quotes.push(doc.data()));
+    if (loggedIn == true) {
+        try {
+            quotes.length = 0;
+            const list = await db.collection("quotes").get();
+            list.forEach(doc => quotes.push(doc.data()));
 
-        if (!req.body.quote && !req.body.author) {
-            return res.status(400).json({message: "You have to pass a quote and/or an author :/"});
-        }
-        const q = await db.collection("quotes").doc(req.params.id).get();
-        if (!q.data()) {
-            return res.status(404).json({message: "Quote not found :("});
-        }
+            if (!req.body.quote && !req.body.author) {
+                return res.status(400).json({ message: "You have to pass a quote and/or an author :/" });
+            }
+            const q = await db.collection("quotes").doc(req.params.id).get();
+            if (!q.data()) {
+                return res.status(404).json({ message: "Quote not found :(" });
+            }
 
-        if (req.body.quote && req.body.author) {
-            db.collection("quotes").doc(req.params.id).set({
-                quote: req.body.quote,
-                author: req.body.author
-            }, {merge: true});
-            return res.json({message: "Updated"});
-        }
-        if (req.body.quote) {
-            db.collection("quotes").doc(req.params.id).set({quote: req.body.quote}, {merge: true});
-            return res.json({message: "Updated"});
-        }
-        if (req.body.author) {
-            db.collection("quotes").doc(req.params.id).set({author: req.body.author}, {merge: true});
-            return res.json({message: "Updated"});
-        }
+            if (req.body.quote && req.body.author) {
+                db.collection("quotes").doc(req.params.id).set({
+                    quote: req.body.quote,
+                    author: req.body.author
+                }, { merge: true });
+                return res.json({ message: "Updated" });
+            }
+            if (req.body.quote) {
+                db.collection("quotes").doc(req.params.id).set({ quote: req.body.quote }, { merge: true });
+                return res.json({ message: "Updated" });
+            }
+            if (req.body.author) {
+                db.collection("quotes").doc(req.params.id).set({ author: req.body.author }, { merge: true });
+                return res.json({ message: "Updated" });
+            }
 
-    } catch (error) {
-        return res.status(500).send(error);
+        } catch (error) {
+            return res.status(500).send(error);
+        }
+    }else{
+        return res.status(401).json({message: "Unauthorized.You're not LoggedIn."});  
     }
 });
 
 router.delete("/quotes/:id", async (req, res) => {
-    try {
+    if (loggedIn == true) {
+        try {
+            const q = await db.collection('quotes').doc(req.params.id).get();
+            if (!q.data()) {
+                return res.status(404).json({ message: "quote not found" });
+            }
 
-        const q = await db.collection('quotes').doc(req.params.id).get();
-        if (!q.data()) {
-            return res.status(404).json({message: "quote not found"});
+            db.collection("quotes").doc(req.params.id).delete();
+
+            return res.status(200).json({ message: "Deleted" });
+
+        } catch (error) {
+            return res.status(500).send(error);
         }
-
-
-        db.collection("quotes").doc(req.params.id).delete();
-
-        return res.status(200).json({message: "Deleted"});
-
-    } catch (error) {
-        return res.status(500).send(error);
+    }else{
+        return res.status(401).json({message: "Unauthorized.You're not LoggedIn."}); 
     }
 });
 
